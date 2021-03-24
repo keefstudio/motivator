c654c675a0d4e3221738bcca7157d8522352bf46
Keef Studio <keefstudio@gmail.com>
2021-03-24 09:28:41 -0500

    pre pwa

---
+++ motivator-app/dist/precache-manifest.e26c617b5f29000ef13fb693ac15957f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887e6a55adb0910b0dfd7e46cb257737",
+    "url": "/checkmark-icon.png"
+  },
+  {
+    "revision": "4c87ae780654bb6f0d3a",
+    "url": "/css/app.fe46c49f.css"
+  },
+  {
+    "revision": "06d40063a9aea1e677c0",
+    "url": "/css/chunk-vendors.d4413b5f.css"
+  },
+  {
+    "revision": "007ec63723842a99a3feb5b084eba3a2",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "9e1f48d150a208e15c32d6746cf82b37",
+    "url": "/fonts/motivator.9e1f48d1.eot"
+  },
+  {
+    "revision": "347c83f7db16b1eff4ceeb79b2ce06dc",
+    "url": "/img/motivator.347c83f7.svg"
+  },
+  {
+    "revision": "f220ff7334c36b1621e1b439b0e1fc84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c87ae780654bb6f0d3a",
+    "url": "/js/app.4b15a63d.js"
+  },
+  {
+    "revision": "06d40063a9aea1e677c0",
+    "url": "/js/chunk-vendors.2a33c35b.js"
+  },
+  {
+    "revision": "4a9a7f30fb1fa9baf96dfcecdd3e682a",
+    "url": "/manifest.json"
+  }
+]);
